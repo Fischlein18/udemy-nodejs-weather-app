5d6f66067c07351b0d1488c5b15f92bf88b5d0c3
D042822 <tao.zhang@sap.com>
2020-03-30 14:47:15 +0200

    Remove uncessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Clientside javascript file is loaded')
-
 //fetch api is not part of nodejs
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
